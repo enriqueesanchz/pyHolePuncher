9577e789dcfaabe529a5b21d15f9c11fa3f82a8e
enriqueesanchz <enrsancar1@alum.us.es>
2023-09-28 23:13:34 +0200

    add setup.py info

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='pyHolePuncher',
+    version='0.1',
+    packages=find_packages(),
+    author='Enrique Sánchez',
+    author_email='enriqueesanchz@gmail.com',
+    description='Simple UDP Hole Punching library in Python',
+    url='https://github.com/enriqueesanchz/pyHolePuncher',
+    license='MIT',
+    keywords='Nat, UDP Hole Puch, Simple'
+)
